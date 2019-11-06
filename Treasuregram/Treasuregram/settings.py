e75a34fa606a63f334da38528e2868b27607edc9
Dhannanjai Nautiyal <dhannanjainautiyal@gmail.com>
2019-11-07 00:49:03 +0530

    Made the view render an HTML template

--- Treasuregram/Treasuregram/settings.py
+++ Treasuregram/Treasuregram/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my apps
+    'main_app',
+    
+    #pre installed apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage_view(request):
-    return HttpResponse('<h1>Welcome to the journey!!!</h1>')
+    return render(request,'homepage_view.html')
