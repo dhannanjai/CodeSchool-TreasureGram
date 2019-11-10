8c748d1985bea1d4b535fda7c8abbe57cd349d0b
Dhannanjai Nautiyal <dhannanjainautiyal@gmail.com>
2019-11-11 00:27:22 +0530

    Made regex expressions possible in urls and displayed details view page for a single treasure

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('',views.homepage_view),
+    re_path(r'^([0-9]+)/$', views.details_view, name='detail'),
 ]
--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -5,3 +5,9 @@ from .models import Treasure
 def homepage_view(request):
     treasures = Treasure.objects.all()
     return render(request,'homepage_view.html', {'treasures':treasures})
+
+
+def details_view(request, treasure_id):
+    treasure = Treasure.objects.get(id = treasure_id)
+    return render(request,'details_view.html',{'treasure':treasure})
+
