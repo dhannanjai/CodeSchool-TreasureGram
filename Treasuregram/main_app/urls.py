ccd751a9aadf1cb82491196ae245e7382c819003
Dhannanjai Nautiyal <dhannanjainautiyal@gmail.com>
2019-11-12 02:16:10 +0530

    Added forms in the frontend

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage_view),
+    path('',views.homepage_view,name='home'),
+    path('post_url',views.post_treasure_view, name='post_treasure'),
     re_path(r'^([0-9]+)/$', views.details_view, name='detail'),
 ]
