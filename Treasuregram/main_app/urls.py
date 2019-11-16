d1598e83361a995598d771eacdaa9e4234bc1d6e
Dhannanjai Nautiyal <dhannanjainautiyal@gmail.com>
2019-11-16 15:42:25 +0530

    added like_treasure url

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     re_path(r'^([0-9]+)/$', views.details_view, name='detail'),
     re_path(r'^user/(\w+)/$',views.user_profile_view, name='profile'),
     path('login/', views.login_view, name='login'),
-    path('logout/',views.logout_view, name='logout')
+    path('logout/',views.logout_view, name='logout'),
+    path('like_treasure/',views.like_treasure_view, name='like_treasure')
 ]
