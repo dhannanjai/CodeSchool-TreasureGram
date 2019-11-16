1ffdb9171483c0b284e1ce8a3088c7783b2f2894
Dhannanjai Nautiyal <dhannanjainautiyal@gmail.com>
2019-11-16 15:43:08 +0530

    added 'likes functionality' into the view

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -53,3 +53,16 @@ def logout_view(request):
     print("Logging out")
     logout(request)
     return HttpResponseRedirect('/')
+
+def like_treasure_view(request):
+    treasure_id=request.GET.get('treasure_id', None)
+
+    likes=0
+    if (treasure_id):
+        treasure = Treasure.objects.get(id=int(treasure_id))
+
+        if treasure is not None:
+            likes = treasure.likes+1
+            treasure.likes=likes
+            treasure.save()
+    return HttpResponse(likes)
